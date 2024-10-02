53466560bc81877f3df08987cfc54c5935b6cc12
Aydensmitty1 <aysm6554@colorado.edu>
2024-10-02 16:24:31 -0600

    MemoryPointersReferences Folder Complete

--- MemoryPointersReferences/MatrixTranspose/mattrans.c
+++ MemoryPointersReferences/MatrixTranspose/mattrans.c
@@ -1,30 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** transposeMatrix(int **matrix, int rows, int cols){
+#define ROWSIZE  4
+#define COLSIZE  5
 
-    return matrix;
+// Prints Matrix to help see results of Transpose
+void printMatrix(int **matrix, int rows, int cols){
+    for (int i = 0; i < rows; i++){
+        printf("ROW: %d|", i);
+        for (int j = 0; j < cols; j++){
+            printf("\t%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return;
 }
 
-void printMatrix(int *matrix){
+// Frees Boths Columns and rows of the matrix
+void deleteMatrix(int ** mat, int rows){
+    for (int i = 0; i < rows; i++){
+        free(mat[i]);
+    }
+    free(mat);
+    return;
+}
 
-    for (int i = 0; i < sizeof(matrix); i++){
-        printf("[");
-        for (int j = 0; j < sizeof(matrix[0]); j++){
-            // printf(" %d,", matrix[i][j]);
+// Populates matrix with by incrementing by one each input
+void populateMatrix(int **mat, int rows, int cols){
+    int count = 0;
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            mat[i][j] = count;
+            count++;
         }
-        printf("]\n");
-
+        
     }
     
 }
-
+// Dynamically allocates Memory and creates a matrix of zeros
+int** matrixInit(int rows, int cols){
+    int **mat = calloc(rows, sizeof(int*));
+    if (!mat){
+        printf("Row Memory Allocation Failed, Quitting.....");
+        return NULL;
+    }
+    
+    for (int i = 0; i < rows; i++){
+       mat[i] = calloc(cols, sizeof(int));
+       if (!mat[i]){
+        printf("Col Memory Allocation Failed, Quitting.....");
+        return NULL;
+    }
+    }
+    return mat;
+}
+// Transposes matrix
+int** transposeMatrix(int **matrix, int rows, int cols){
+    int **newMat;
+    newMat = matrixInit(COLSIZE, ROWSIZE);
+    for (int i = 0; i < cols; i++){
+        for (int j = 0; j < rows; j++){
+            newMat[i][j] = matrix[j][i];
+        }
+    }
+    deleteMatrix(matrix, cols);
+    return newMat;
+}
 
 int main(){
-    int rows, cols = 3;
-    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    printMatrix(matrix);
-    transposeMatrix(&matrix, rows, cols);
-    printMatrix(matrix);
+    int **matrix;
+    matrix = matrixInit(ROWSIZE, COLSIZE);
+    populateMatrix(matrix, ROWSIZE, COLSIZE);
+    printMatrix(matrix, ROWSIZE, COLSIZE);
+    matrix = transposeMatrix(matrix, ROWSIZE, COLSIZE);
+    printf("----------------TRANSPOSED-------------------\n");
+    printMatrix(matrix, COLSIZE, ROWSIZE);
+    matrix = transposeMatrix(matrix, COLSIZE, ROWSIZE);
+    printf("----------------UNTRANSPOSED-----------------\n");
+    printMatrix(matrix, ROWSIZE, COLSIZE);
+    deleteMatrix(matrix, ROWSIZE);
     return 0;
 }
